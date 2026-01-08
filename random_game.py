181b3e8211d62710c3159f4122501081945697a9
Ezejioha Nnamdi <nnamdi@example.com>
2026-01-08 17:45:12 +0100

    updated game 2 properly

--- random_game.py
+++ random_game.py
@@ -8,12 +8,6 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-# Create a "placeholder" with the same number of blanks as the chosen_word
-placeholder = ""
-word_length = len(chosen_word)
-for position in range(word_length):
-    placeholder += "_"
-print(placeholder)
 
 guess = input("Guess a letter: \n").lower()
 
@@ -26,14 +20,5 @@ for letter in chosen_word:
     else:
         print("Wrong")
 
-#Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
 
-display = ""
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
-
-        
